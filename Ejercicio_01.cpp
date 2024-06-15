b6110eaf6edb8598cbebece816b5c31aeb97365c
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-14 23:15:36 -0500

    Actualizando y ordenando el ejercicio 01

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main() {
     int n, contador, multiplo;
+    
     contador = 1;
     multiplo = 7;
 
-    cout << "Ingrese el valor de n: ";
+    cout << "Ingrese el valor de n: (para determinar sus multiplos) ";
     cin >> n;
 
     if (n <= 0) {
-        cout << "El valor de n debe ser mayor que cero." << endl;
+        cout << "El valor de n debe ser mayor que cero, vuelva a intentarlo" << endl;
     } else {
         cout << "Los primeros " << n << " multiplos de 7 son:" << endl;
         while (contador <= n) {
@@ -21,5 +22,6 @@ int main() {
     }
 
     return 0;
+    
 }
 
