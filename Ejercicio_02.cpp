db6cd3cdc0c225630e34948d6b9c4dc43828ebf1
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-15 20:19:01 -0500

    Actualizar y añadir un contador en ejercicio 02

--- Ejercicio_02.cpp
+++ Ejercicio_02.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int main() {
-    double numero;
+	
+    double numero, contador;
     cout << "Introduce un numero (introduce uno negativo para finalizar): "<<endl;
     cin >> numero;
+    contador=1;
 
-    while (numero >= 0) {
+    while(numero >= 0){
         cout << "Introduce otro numero (negativo para finalizar): "<<endl;
         cin >> numero;
+        contador=contador+1;
     }
 
-    cout << "Has introducido un numero negativo. Fin del programa" << endl;
+    cout << "Has introducido un numero negativo. En total has ingresado "<< contador <<" numeros." <<endl;
 
     return 0;
+    
 }
 
