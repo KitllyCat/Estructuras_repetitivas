56825b55745be458d2c01629beb4a3de88913b31
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-17 23:05:51 -0500

    Actualizando y reemplazando el for por do while, ejercicio 04

--- Ejercicio_04.cpp
+++ Ejercicio_04.cpp
@@ -2,33 +2,28 @@
 using namespace std;
 
 int main() {
-    int n;
-    double totalGastos = 0.0, gasto;
+    int n, contador;
+    double totalGastos, gasto;
     char continuar;
 
+    totalGastos = 0;
+	contador = 0;
+	
     cout << "Ingrese la cantidad de gastos: " << endl;
     cin >> n;
 
-    for (int i = 0; i < n; ++i) {
-        cout << "Ingrese el gasto " << i + 1 << ": " << endl;
+    do {
+        contador++;
+        cout << "Ingrese el gasto " << contador << " : " << endl;
         cin >> gasto;
-        totalGastos += gasto;
-    }
-
-    cout << "¿Desea continuar ingresando más gastos? (S/N): ";
-    cin >> continuar;
-
-    while (continuar == 'S' || continuar == 's') {
-        cout << "Ingrese el siguiente gasto: ";
-        cin >> gasto;
+        
         totalGastos += gasto;
 
         cout << "¿Desea continuar ingresando más gastos? (S/N): ";
         cin >> continuar;
-    }
+    } while (continuar == 'S' || continuar == 's');
 
     cout << "La suma total de los gastos es: " << totalGastos << endl;
 
     return 0;
 }
-
