54370cd7d818256f30d2e95ae14c544f6fe0cf2e
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-18 23:04:25 -0500

    Actualizar ejercicio 05, ordenar y separar el if y while

--- Ejercicio_05.cpp
+++ Ejercicio_05.cpp
@@ -3,40 +3,43 @@
 using namespace std;
 
 int main() {
+	
     int n, aprobados, desaprobados, i;
     double sumaNotas, nota, promedio;
-
-    cout << "Ingrese la cantidad de estudiantes (n): " << endl;
-    cin >> n;
-    
-    aprobados = 0;
+	
+	aprobados = 0;
     desaprobados = 0;
     sumaNotas = 0;
     i = 1;
+	
+    cout << "Ingrese la cantidad de estudiantes (n): " << endl;
+    cin >> n;
     
     if (n <= 0) {
-        cout << "La cantidad de estudiantes debe ser mayor que cero." << endl;
-    } else {
-        while (i <= n) {
-            cout << "Ingrese la nota del estudiante " << i << ": ";
-            cin >> nota;
-
-            if (nota >= 10) {
-                aprobados++;
-            } else {
-                desaprobados++;
-            }
-
-            sumaNotas += nota;
-            i++;
-        }
-        promedio = sumaNotas / n;
-
-        cout << "La cantidad de estudiantes aprobados es de: " << aprobados << endl;
-        cout << "La cantidad de estudiantes desaprobados es de: " << desaprobados << endl;
-        cout << "El promedio de notas del salón es: " << promedio << endl;
+    cout << "La cantidad de estudiantes debe ser mayor que cero." << endl;
+    return 1;
     }
 
+    while (i <= n) {
+        cout << "Ingrese la nota del estudiante número " << i << " : ";
+        cin >> nota;
+
+        if (nota >= 10) {
+            aprobados++;
+        } else {
+            desaprobados++;
+        }
+
+        sumaNotas += nota;
+        i++;
+    }
+    
+    promedio = sumaNotas / n;
+
+    cout << "La cantidad de estudiantes aprobados es de: " << aprobados << ", y la cantidad de desaprobados es de: " << desaprobados << endl;
+    cout << "El promedio de notas del salón es: " << promedio << endl;
+
     return 0;
+    
 }
 
